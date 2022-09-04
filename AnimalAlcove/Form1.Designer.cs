500243caafa363e0d5f77ba6dc9f5f96bc4b29de
MiitaliGupta <mitaligupta2103@gmail.com>
2022-09-04 17:08:15 +0530

    updated color n font

--- AnimalAlcove/Form1.Designer.cs
+++ AnimalAlcove/Form1.Designer.cs
@@ -38,11 +38,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Ravie", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Font = new System.Drawing.Font("Rockwell Extra Bold", 55F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
             this.label1.Location = new System.Drawing.Point(116, 49);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(798, 107);
+            this.label1.Size = new System.Drawing.Size(830, 108);
             this.label1.TabIndex = 0;
             this.label1.Text = "Animal Alcove";
             // 
@@ -52,7 +52,7 @@
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Stencil", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button1.ForeColor = System.Drawing.Color.Maroon;
             this.button1.Location = new System.Drawing.Point(149, 416);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(251, 100);
@@ -67,7 +67,7 @@
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Stencil", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button2.ForeColor = System.Drawing.Color.Maroon;
             this.button2.Location = new System.Drawing.Point(619, 416);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(278, 100);
